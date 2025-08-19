435201436fc74bcda045071040a186517dc5498b
Ewan Fraser <ewanf@outlook.com>
2025-08-19 16:00:29 +0100

    Create send data function that sends documents from python api to elasticsearch

--- shipper.py
+++ shipper.py
@@ -1,18 +1,47 @@
-from flask import Flask, request
+import random
 import urllib.request, urllib.parse
+from elasticsearch import Elasticsearch, helpers
+import random
 import schedule
+import json
+from faker import Faker
 schema = "Logs"
-number_of_docs = "1"
-url ="http://54.210.181.24:31000/Schemas/" +schema +"/data?" + number_of_docs
-print(url)
+number_of_docs = "10"
+url ="http://54.210.181.24:31000/Schemas/" +schema +"/data?no=" + number_of_docs
+headers = {"Accept":"application/json"}
+client = Elasticsearch("https://54.210.181.24:9200",api_key="SWxaM3dwZ0JOWUhzMHA1RXQ3aE06TzBBd2RJSkE4bGpwVG84RjdLeHhUdw==",verify_certs=False)
+#print(url)
 
-def get_data(url):
-    request_url = urllib.request.Request(url)
-    response = urllib.request.urlopen(request_url)
-    print(response.read())
+def get_data(url,headers):
+    request_url = urllib.request.Request(url, headers=headers)
+    with urllib.request.urlopen(request_url) as response:
+        data = response.read().decode('utf-8')
+    return data
 
-schedule.every(15).seconds.do(get_data, url)
+
+def send_data(url,headers):
+    data = get_data(url,headers=headers)
+    index =schema.lower()
+    if not client.indices.exists(index=index):
+        client.indices.create(index=index)
+    data=json.loads(data)
+    for i in data:
+        item = json.dumps(i)
+        client.index(index=index, id=str(random.randint(0,10000)), body=item)
+
+
+
+
+    print("Data sent to elasticsearch:" + json.dumps(data))
+
+
+
+
+
+schedule.every(10).seconds.do(send_data, url,headers)
 
 while True:
     schedule.run_pending()
+
+
 
